4c08b538464b9242fb1f9780306757a5ec772072
Daniel Condly <dcond007@fiu.edu>
2022-01-03 14:13:23 -0500

    First Commit

--- RGB Color Gen/ViewController.swift
+++ RGB Color Gen/ViewController.swift
@@ -8,12 +8,85 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var viewBackground: UIView!
+    @IBOutlet weak var colorCodeLabel: UILabel!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+   @IBAction func changeColorComponent(_ sender: AnyObject) {
+        
+       let red: CGFloat = CGFloat(self.redSlider.value)
+       let redInt = Int(red * 100)
+       
+       let blue: CGFloat = CGFloat(self.blueSlider.value)
+       let blueInt = Int(blue * 100)
+       
+       let green: CGFloat = CGFloat(self.greenSlider.value)
+       let greenInt = Int(green * 100)
+       
+       let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+       viewBackground.backgroundColor = color
 
+       // Labels
+       redLabel.text = "Red\t\(redInt)%\t\(Int(255*red))"
+       greenLabel.text = "Green\t\(greenInt)%\t\(Int(255*green))"
+       blueLabel.text = "Blue\t\(blueInt)%\t\(Int(255*blue))"
+       colorCodeLabel.text = self.hexStringFromColor(color: color)
+
+    }
+    
+    func hexStringFromColor(color: UIColor) -> String {
+        let components = color.cgColor.components
+        let r: CGFloat = components?[0] ?? 0.0
+        let g: CGFloat = components?[1] ?? 0.0
+        let b: CGFloat = components?[2] ?? 0.0
+
+        let hexString = String.init(format: "#%02lX%02lX%02lX", lroundf(Float(r * 255)), lroundf(Float(g * 255)), lroundf(Float(b * 255)))
+        print(hexString)
+        return hexString
+     }
+
+    func colorWithHexString(hexString: String) -> UIColor {
+        var colorString = hexString.trimmingCharacters(in: .whitespacesAndNewlines)
+        colorString = colorString.replacingOccurrences(of: "#", with: "").uppercased()
+
+        print(colorString)
+        let alpha: CGFloat = 1.0
+        let red: CGFloat = self.colorComponentFrom(colorString: colorString, start: 0, length: 2)
+        let green: CGFloat = self.colorComponentFrom(colorString: colorString, start: 2, length: 2)
+        let blue: CGFloat = self.colorComponentFrom(colorString: colorString, start: 4, length: 2)
+
+        let color = UIColor(red: red, green: green, blue: blue, alpha: alpha)
+        return color
+    }
+
+    func colorComponentFrom(colorString: String, start: Int, length: Int) -> CGFloat {
+
+        let startIndex = colorString.index(colorString.startIndex, offsetBy: start)
+        let endIndex = colorString.index(startIndex, offsetBy: length)
+        let subString = colorString[startIndex..<endIndex]
+        let fullHexString = length == 2 ? subString : "\(subString)\(subString)"
+        var hexComponent: UInt32 = 0
+
+        guard Scanner(string: String(fullHexString)).scanHexInt32(&hexComponent) else {
+            return 0
+        }
+        let hexFloat: CGFloat = CGFloat(hexComponent)
+        let floatValue: CGFloat = CGFloat(hexFloat / 255.0)
+        print(floatValue)
+        return floatValue
+    }
 }
 
