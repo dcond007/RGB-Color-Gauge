6e9c4c4b9ec0dd2fafac5db6fc99248c8c5dc86c
Daniel Condly <dcond007@fiu.edu>
2022-01-03 11:34:20 -0500

    Initial Commit

---
+++ RGB Color Gen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGB Color Gen
+//
+//  Created by Astronaut Elvis on 1/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RGB Color GenUITests/RGB_Color_GenUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RGB_Color_GenUITestsLaunchTests.swift
+//  RGB Color GenUITests
+//
+//  Created by Astronaut Elvis on 1/3/22.
+//
+
+import XCTest
+
+class RGB_Color_GenUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
